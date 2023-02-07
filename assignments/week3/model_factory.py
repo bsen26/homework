8a99c73ff73ff5110089c84c47e09ad5ff4e475c
Bhaskar Sen <senbhaskar26@gmail.com>
2023-02-07 10:13:08 -0500

    Keep dropout at 0.5, change hidden units to 1024

--- assignments/week3/model_factory.py
+++ assignments/week3/model_factory.py
@@ -15,4 +15,4 @@ def create_model(input_dim: int, output_dim: int) -> MLP:
         MLP: The created model.
 
     """
-    return MLP(input_dim, 100, output_dim, 2, torch.nn.ReLU, torch.nn.init.zeros_)
+    return MLP(input_dim, 1024, output_dim, 2, torch.nn.ReLU, torch.nn.init.zeros_)
